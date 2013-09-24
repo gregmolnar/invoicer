2c3570bf8d5ef22aec0262608aa5548ef2a5ae19
Greg Molnar <molnargerg@gmail.com>
2013-09-24 18:52:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Invoicer::Application.config.secret_key_base = 'ea30ee27c966d4cf7897d67220f2c3263244749bc8c736a5a3d463096d1e2d2e6bdc341d88e11c1e08da6d30553b5be931a0121f7b8451b216eda1f0de480b25'
