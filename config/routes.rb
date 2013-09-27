047795aff9917ea49e724b5257161b2a58273ae5
Greg Molnar <molnargerg@gmail.com>
2013-09-27 16:26:03 +0200

    login as root path

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Invoicer::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
