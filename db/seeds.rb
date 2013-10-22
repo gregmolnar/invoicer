41b846f800e6372a50f7db6df084eea328670410
Greg Molnar <molnargerg@gmail.com>
2013-10-22 21:30:28 +0200

    add invoice statuses to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+InvoiceStatus.create([{name: 'Draft'}, {name: 'Sent'}, {name: 'Paid'}, {name: 'Void'}])
