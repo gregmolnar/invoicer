1bb224ed4f643a39255710bb282ada309b440646
Greg Molnar <molnargerg@gmail.com>
2013-11-24 18:23:33 +0100

    group invoice items

--- app/controllers/invoice_items_controller.rb
+++ app/controllers/invoice_items_controller.rb
@@ -1,5 +1,5 @@
 class InvoiceItemsController < ApplicationController
   def index
-    @invoice_items = InvoiceItem.where("name like ?", "%#{params[:term]}%")
+    @invoice_items = InvoiceItem.where("name like ?", "%#{params[:term]}%").select(:name, :price, :unit, :description).group([:name, :price, :unit, :description])
   end
 end
