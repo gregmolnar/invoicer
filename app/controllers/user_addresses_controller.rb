8a53d9fe3534ce31f12415120e3399235fb27569
Greg Molnar <molnargerg@gmail.com>
2013-12-04 21:35:06 +0100

    use inherited resource's url helpers

--- app/controllers/user_addresses_controller.rb
+++ app/controllers/user_addresses_controller.rb
@@ -1,6 +1,6 @@
 class UserAddressesController < ApplicationController
   inherit_resources
-  defaults resource_class: Address, :collection_name => 'addresses', :instance_name => 'address'
+  defaults resource_class: Address, :collection_name => 'addresses', :instance_name => 'address', :route_instance_name => 'address'
   belongs_to :user
   before_filter :set_view_path
 
