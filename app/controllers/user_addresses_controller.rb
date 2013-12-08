1155829935b21060d5f04238127bff9694526318
Greg Molnar <molnargerg@gmail.com>
2013-12-08 17:45:22 +0100

    redirect to index after address save

--- app/controllers/user_addresses_controller.rb
+++ app/controllers/user_addresses_controller.rb
@@ -1,8 +1,16 @@
 class UserAddressesController < ApplicationController
   inherit_resources
-  defaults resource_class: Address , :collection_name => 'addresses', :instance_name => 'address', :route_instance_name => 'address'
+  defaults resource_class: Address , :collection_name => 'addresses', :instance_name => 'address', :route_instance_name => 'address', :route_collection_name => 'addresses'
   belongs_to :user
   before_filter :set_view_path
+
+  def create
+    create!{ collection_path }
+  end
+
+  def update
+    update!{ collection_path }
+  end
 
   def set_view_path
     @_prefixes = 'addresses'
